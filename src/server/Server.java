4899873c3df7d33c1c5b7f9d07fc0ebb170fdc2a
Davide Fossacecchi <rocker.rabbit@gmail.com>
2015-02-21 17:19:54 +0100

    Add a first implementation with connection sockets and threads

---
+++ src/server/Server.java
@@ -0,0 +1,31 @@
+package server;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import chatthreads.*;
+
+public class Server {
+
+	public static void main(String[] args) {
+		try{
+			ServerSocket serverSocket = new ServerSocket(9999);
+			Socket acceptSocket = serverSocket.accept();
+			PrintStream output = new PrintStream(acceptSocket.getOutputStream());
+			BufferedReader input = new BufferedReader(new InputStreamReader(acceptSocket.getInputStream()));
+			ReceiveThread inbox = new ReceiveThread(acceptSocket, input);
+			SendThread dispatch = new SendThread(acceptSocket, output);
+			
+			inbox.start();
+			dispatch.start();
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+
+	}
+
+}
